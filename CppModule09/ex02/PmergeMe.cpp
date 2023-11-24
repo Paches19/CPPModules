9523ed6130dcbde61e42d22ddfdbc330e2026c96
root <root@LAPTOP-UTKVD2O1>
2023-11-24 13:35:29 +0100

    cpp9 end

--- CppModule09/ex02/PmergeMe.cpp
+++ CppModule09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 09:48:49 by adpachec          #+#    #+#             */
-/*   Updated: 2023/11/23 13:27:35 by adpachec         ###   ########.fr       */
+/*   Updated: 2023/11/24 13:33:03 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,14 +99,14 @@ void PmergeMe::sortAndPrint()
 		return ;
 	}
 	// printContainer(_numbersDeque, "std::deque", 1);
-	// start = clock();
-	// mergeInsertionSort(_numbersDeque);
-	// end = clock();
-	// timeToExecute = ((double) (end - start)) / CLOCKS_PER_SEC;
-	// printContainer(_numbersDeque, "std::deque", 0);
-	// std::cout << "Time to process a range of " << _numbersDeque.size() <<
-	// 	" elements with std::deque: " << timeToExecute << " s\n";
-	// std::cout << std::endl;
+	start = clock();
+	mergeInsertionSort(_numbersDeque);
+	end = clock();
+	timeToExecute = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printContainer(_numbersDeque, "std::deque", 0);
+	std::cout << "Time to process a range of " << _numbersDeque.size() <<
+		" elements with std::deque: " << timeToExecute << " s\n";
+	std::cout << std::endl;
 	
 	// printContainer(_numbersList, "std::list", 1);
 	start = clock();
@@ -117,166 +117,308 @@ void PmergeMe::sortAndPrint()
 	std::cout << "Time to process a range of " << _numbersList.size() << " elements with std::list: " << timeToExecute << " s\n";
 }
 
-template<typename T>
-void PmergeMe::merge(T& left, T& right)
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// 						FUNCIONES LISTA								//
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// // // // // // // // // // // // // // // // // // // // // // // // 
+
+// Función para formar pares de elementos
+void PmergeMe::formPairs(std::list<int>& container, std::list<Pair>& pairs, std::list<long>& singleElements)
 {
-	T result;
-	while (!left.empty() && !right.empty())
-	{
-		if (left.front() <= right.front())
-		{
-			result.push_back(left.front());
-			left.pop_front();
-		}
-		else
-		{
-			result.push_back(right.front());
-			right.pop_front();
-		}
-	}
-	while (!left.empty())
-	{
-		result.push_back(left.front());
-		left.pop_front();
-	}
-	while (!right.empty())
-	{
-		result.push_back(right.front());
-		right.pop_front();
-	}
-	left = result;
-}
-
-template<typename T>
-void PmergeMe::mergeSort(T& container)
-{
-	if (container.size() <= 1)
-		return;
-
-	typename T::iterator middle = container.begin();
-	std::advance(middle, container.size() / 2);
-	
-	T left(container.begin(), middle);
-	T right(middle, container.end());
-	
-	mergeSort(left);
-	mergeSort(right);
-	
-	merge(left, right);
-	
-	container = left;
-}
-
-template<typename T>
-void PmergeMe::mergeInsertionSort(T& container)
-{
-if (container.empty() || isSorted(container)) {
-		return; // El contenedor ya está ordenado o está vacío.
-	}
-
-	// Paso 1: Dividir en pares y ordenar cada par
-	std::list<int> mainChain;
-	std::list<int> pendElements;
-
-	typename T::iterator it = container.begin();
+	std::list<int>::iterator it = container.begin();
 	while (it != container.end())
 	{
-		int first = *it;
-		++it;
-		if (it != container.end()) {
-			int second = *it;
-			++it;
-			mainChain.push_back(std::min(first, second));
-			pendElements.push_back(std::max(first, second));
-		} else {
-			mainChain.push_back(first); // Si hay un número impar de elementos
-		}
-	}
-	mergeSort(mainChain);
-	// std::cout << "Main Chain "<< std::endl;
-	// std::copy(mainChain.begin(), mainChain.end(), std::ostream_iterator<int>(std::cout, " "));
-	// std::cout << std::endl;
-	// std::cout << "Pend Chain "<< std::endl;
-	// std::copy(pendElements.begin(), pendElements.end(), std::ostream_iterator<int>(std::cout, " "));
-	// std::cout << std::endl;
-	// Paso 2: Insertar los elementos pendientes en la cadena principal
-    while (!pendElements.empty())
-	{
-        std::list<int>::iterator lowestCostElementIt = pendElements.begin();
-        int lowestCost = calculateInsertionCost(pendElements, *lowestCostElementIt);
-        // Encuentra el elemento con el coste más bajo
-        for (std::list<int>::iterator it2 = ++pendElements.begin(); it2 != pendElements.end(); ++it2)
+		int first = *it++;
+		if (it != container.end())
 		{
-            int cost = calculateInsertionCost(pendElements, *it2);
-            if (cost < lowestCost)
-			{
-                lowestCost = cost;
-                lowestCostElementIt = it2;
-            }
-        }
-        // Encuentra la posición de inserción en mainChain para el elemento seleccionado
-        std::list<int>::iterator insertPos = findInsertPosition(mainChain, *lowestCostElementIt);
-        // Inserta el elemento en la posición encontrada
-        mainChain.insert(insertPos, *lowestCostElementIt);
-        // Elimina el elemento de pendElements
-        pendElements.erase(lowestCostElementIt);
-		// std::cout << "Main Chain "<< std::endl;
-		// std::copy(mainChain.begin(), mainChain.end(), std::ostream_iterator<int>(std::cout, " "));
-		// std::cout << std::endl;
-		// std::cout << "Pend Chain "<< std::endl;
-		// std::copy(pendElements.begin(), pendElements.end(), std::ostream_iterator<int>(std::cout, " "));
-		// std::cout << std::endl;
-    }
+			int second = *it++;
+			// Asegúrate de que 'first' es el mayor y 'second' es el menor
+			if (first < second) std::swap(first, second);
+				pairs.push_back(Pair(first, second));
+		} else
+			// Si el número de elementos es impar, guarda el elemento solitario
+			singleElements.push_back(first);
+	}
+}
 
-	// Paso 3: Reconstruir el arreglo ordenado
-	container.clear();
-	std::copy(mainChain.begin(), mainChain.end(), std::back_inserter(container));
+void PmergeMe::merge(std::list<Pair>& left, std::list<Pair>& right, std::list<Pair>& result)
+{
+    while (!left.empty() && !right.empty())
+	{
+        if (left.front().a <= right.front().a)
+		{
+            result.push_back(left.front());
+            left.pop_front();
+        }
+		else
+		{
+            result.push_back(right.front());
+            right.pop_front();
+        }
+    }
+    while (!left.empty())
+	{
+        result.push_back(left.front());
+        left.pop_front();
+    }
+    while (!right.empty())
+	{
+        result.push_back(right.front());
+        right.pop_front();
+    }
+}
+
+void PmergeMe::mergeSort(std::list<Pair>& container)
+{
+    if (container.size() <= 1) return;
+
+    std::list<Pair>::iterator middle = container.begin();
+    std::advance(middle, container.size() / 2);
+
+    std::list<Pair> left(container.begin(), middle);
+    std::list<Pair> right(middle, container.end());
+
+    mergeSort(left);
+    mergeSort(right);
+
+    container.clear();
+    merge(left, right, container);
+}
+
+void PmergeMe::insertInMainChain(std::list<int>& mainChain, std::list<int>& pendElements)
+{
+	std::list<int> jacobsthalSequence = PmergeMe::generateJacobsthalSequence<std::list<int> >(pendElements.size());
+
+	int lastIndx = 0;
+	for (std::list<int>::iterator jIt = jacobsthalSequence.begin(); jIt != jacobsthalSequence.end(); ++jIt)
+	{
+		if (*jIt == lastIndx)
+			continue ;
+		int idx = *jIt;
+		if (idx >= static_cast<int>(pendElements.size()))
+			idx = static_cast<int>(pendElements.size() - 1);
+		std::list<int>::iterator itPend = pendElements.begin();
+		std::advance(itPend, idx);
+		
+		int pendValue = *itPend;
+		std::list<int>::iterator itMain = findInsertPosition(mainChain, pendValue);
+		mainChain.insert(itMain, pendValue);
+		// Insertar elementos anteriores no insertados
+		while (--idx > lastIndx || (lastIndx == 0 && idx == 0))
+		{
+			itPend = pendElements.begin();
+			std::advance(itPend, idx);
+			pendValue = *itPend;
+			itMain = findInsertPosition(mainChain, pendValue);
+			mainChain.insert(itMain, pendValue);
+		}
+		lastIndx = *jIt;
+		if (*jIt >= static_cast<int>(pendElements.size()))
+			break ;
+	}
+}
+
+void PmergeMe::mergeInsertionSort(std::list<int>& container)
+{
+    if (container.empty() || isSorted(container))
+        return; // El contenedor ya está ordenado o está vacío.
+
+    // Paso 1: Crear pares
+    std::list<Pair> pairs;
+    std::list<int>::iterator it = container.begin();
+    while (it != container.end())
+	{
+        long first = *it++;
+        long second = (it != container.end()) ? *it++ : (long) INT_MIN - 1;
+        pairs.push_back(Pair(first > second ? first : second, first > second ? second : first));
+    }
+    // Ordena los pares por 'ak'
+    mergeSort(pairs);
+    // Separar los pares ordenados en 'mainChain' y 'pendElements'
+    std::list<int> mainChain;
+    std::list<int> pendElements;
+    for (std::list<Pair>::iterator pairIt = pairs.begin(); pairIt != pairs.end(); ++pairIt)
+	{
+		if (pairIt->a <= INT_MAX && pairIt->a >= 0)
+        	mainChain.push_back(pairIt->a);
+        if (pairIt->b <= INT_MAX && pairIt->b >= 0)
+            pendElements.push_back(pairIt->b);
+    }
+	// Paso 4: Insertar los elementos pendientes en la cadena principal
+    insertInMainChain(mainChain, pendElements);
+    container = mainChain;
 }
 
 std::list<int>::iterator PmergeMe::findInsertPosition(std::list<int>& mainChain, int value)
 {
     std::list<int>::iterator it = mainChain.begin();
-    while (it != mainChain.end() && *it < value)
+    while (it != mainChain.end() && *it <= value)
         ++it;
     return it;
 }
 
-int PmergeMe::calculateInsertionCost(const std::list<int>& pendElements, int value)
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// 						FUNCIONES DEQUE								//
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// // // // // // // // // // // // // // // // // // // // // // // // 
+
+// Función para formar pares de elementos
+void PmergeMe::formPairs(std::deque<int>& container, std::deque<Pair>& pairs, std::deque<long>& singleElements)
 {
-    // Calcula el número de movimientos para llevar 'value' al inicio o al final de la cadena pendElements
-    int movesToFront = 0;
-    int movesToEnd = 0;
-    for (std::list<int>::const_iterator it = pendElements.begin(); it != pendElements.end(); ++it) {
-        if (*it == value) break;
-        movesToFront++;
-    }
-    for (std::list<int>::const_reverse_iterator rit = pendElements.rbegin(); rit != pendElements.rend(); ++rit) {
-        if (*rit == value) break;
-        movesToEnd++;
-    }
-    // El coste de inserción es el menor número de movimientos necesario
-    return std::min(movesToFront, movesToEnd);
+	std::deque<int>::iterator it = container.begin();
+	while (it != container.end()) {
+		int first = *it++;
+		if (it != container.end()) {
+			int second = *it++;
+			// Asegúrate de que 'first' es el mayor y 'second' es el menor
+			if (first < second) std::swap(first, second);
+			pairs.push_back(Pair(first, second));
+		} else {
+			// Si el número de elementos es impar, guarda el elemento solitario
+			singleElements.push_back(first);
+		}
+	}
 }
 
-void PmergeMe::resetJacobsthal()
+void PmergeMe::merge(std::deque<Pair>& left, std::deque<Pair>& right, std::deque<Pair>& result)
 {
-	secondLastJacobsthal = 0;
-	lastJacobsthal = 1;
+    while (!left.empty() && !right.empty())
+	{
+        if (left.front().a <= right.front().a)
+		{
+            result.push_back(left.front());
+            left.pop_front();
+        }
+		else
+		{
+            result.push_back(right.front());
+            right.pop_front();
+        }
+    }
+    while (!left.empty())
+	{
+        result.push_back(left.front());
+        left.pop_front();
+    }
+    while (!right.empty())
+	{
+        result.push_back(right.front());
+        right.pop_front();
+    }
 }
 
-int PmergeMe::calculateNextJacobsthal() {
-    if (lastJacobsthal == 0 && secondLastJacobsthal == 0) {
-        // Inicializar los dos primeros números de Jacobsthal
-        secondLastJacobsthal = 0; // J(0)
-        lastJacobsthal = 1;       // J(1)
-        return 1;
-    } else {
-        // Calcular el próximo número de Jacobsthal
-        int nextJacobsthal = lastJacobsthal + 2 * secondLastJacobsthal;
-        secondLastJacobsthal = lastJacobsthal;
-        lastJacobsthal = nextJacobsthal;
-        return nextJacobsthal;
+void PmergeMe::mergeSort(std::deque<Pair>& container)
+{
+    if (container.size() <= 1) return;
+
+    std::deque<Pair>::iterator middle = container.begin();
+    std::advance(middle, container.size() / 2);
+
+    std::deque<Pair> left(container.begin(), middle);
+    std::deque<Pair> right(middle, container.end());
+
+    mergeSort(left);
+    mergeSort(right);
+
+    container.clear();
+    merge(left, right, container);
+}
+
+void PmergeMe::insertInMainChain(std::deque<int>& mainChain, std::deque<int>& pendElements)
+{
+	std::deque<int> jacobsthalSequence = PmergeMe::generateJacobsthalSequence<std::deque<int> >(pendElements.size());
+
+	int lastIndx = 0;
+	for (std::deque<int>::iterator jIt = jacobsthalSequence.begin(); jIt != jacobsthalSequence.end(); ++jIt)
+	{
+		if (*jIt == lastIndx)
+			continue ;
+		int idx = *jIt;
+		if (idx >= static_cast<int>(pendElements.size()))
+			idx = static_cast<int> (pendElements.size() - 1);
+
+		int pendValue = pendElements[idx];
+		int insertPos = findInsertPosition(mainChain, pendValue);
+        mainChain.insert(mainChain.begin() + insertPos, pendValue);
+
+        // Inserta elementos pendientes anteriores que no se han insertado aún
+        while (--idx > lastIndx || (lastIndx == 0 && idx == 0))
+		{
+            pendValue = pendElements[idx];
+            insertPos = findInsertPosition(mainChain, pendValue);
+            mainChain.insert(mainChain.begin() + insertPos, pendValue);
+        }
+		// Actualiza el índice del último elemento insertado
+        lastIndx = *jIt;
+		if (*jIt >= static_cast<int>(pendElements.size()))
+			break ;
     }
+}
+
+void PmergeMe::mergeInsertionSort(std::deque<int>& container)
+{
+    if (container.empty() || isSorted(container))
+        return; // El contenedor ya está ordenado o está vacío.
+
+    // Paso 1: Crear pares
+    std::deque<Pair> pairs;
+    std::deque<int>::iterator it = container.begin();
+    while (it != container.end())
+	{
+        long first = *it++;
+        long second = (it != container.end()) ? *it++ : (long) INT_MIN - 1;
+        pairs.push_back(Pair(first > second ? first : second, first > second ? second : first));
+    }
+    // Ordena los pares por 'ak'
+    mergeSort(pairs);
+    // Separar los pares ordenados en 'mainChain' y 'pendElements'
+    std::deque<int> mainChain;
+    std::deque<int> pendElements;
+    for (std::deque<Pair>::iterator pairIt = pairs.begin(); pairIt != pairs.end(); ++pairIt)
+	{
+		if (pairIt->a <= INT_MAX && pairIt->a >= 0)
+        	mainChain.push_back(pairIt->a);
+        if (pairIt->b <= INT_MAX && pairIt->b >= 0)
+            pendElements.push_back(pairIt->b);
+    }
+	// Paso 4: Insertar los elementos pendientes en la cadena principal
+    insertInMainChain(mainChain, pendElements);
+    container = mainChain;
+}
+
+int PmergeMe::findInsertPosition(std::deque<int>& mainChain, int value)
+{
+    for (int i = 0; i < static_cast<int>(mainChain.size()); i++)
+	{
+        if (mainChain[i] >= value)
+            return i;
+    }
+	return static_cast<int>(mainChain.size());
+}
+
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// 						FUNCIONES TEMPLATE							//
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// // // // // // // // // // // // // // // // // // // // // // // // 
+
+template<typename T>
+T PmergeMe::generateJacobsthalSequence(size_t size)
+{
+    T sequence;
+    int jacobsthalPrev = 0;
+    int jacobsthalCurr = 1;
+
+    while (sequence.size() < size)
+	{
+        sequence.push_back(jacobsthalPrev);
+        int nextJacobsthal = jacobsthalCurr + 2 * jacobsthalPrev;
+        jacobsthalPrev = jacobsthalCurr;
+        jacobsthalCurr = nextJacobsthal;
+    }
+    return sequence;
 }
 
 template<typename T>
--- CppModule09/ex02/PmergeMe.hpp
+++ CppModule09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 09:48:54 by adpachec          #+#    #+#             */
-/*   Updated: 2023/11/23 13:25:05 by adpachec         ###   ########.fr       */
+/*   Updated: 2023/11/24 12:23:56 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,13 @@
 # include <limits.h>
 # include <cmath>
 # include <iterator>
-# include "BinarySearchTree.hpp"
+
+struct Pair
+{
+    long a; // El elemento 'a' del par, que será el menor de los dos
+    long b; // El elemento 'b' del par, que será el mayor de los dos
+	Pair(long a, long b) : a(a), b(b) {};
+};
 
 class PmergeMe
 {
@@ -30,19 +36,22 @@ class PmergeMe
 		std::list<int> _numbersList;
 		bool _success;
 
-		int lastJacobsthal;
-        int secondLastJacobsthal;
-
 		static bool isNumber(const std::string& str);
+		
+		void formPairs(std::list<int>& container, std::list<Pair>& pairs, std::list<long>& singleElements);
+		void merge(std::list<Pair>& left, std::list<Pair>& right, std::list<Pair>& result);
+		void mergeSort(std::list<Pair>& container);
+		void insertInMainChain(std::list<int>& mainChain, std::list<int>& pendElements);
+		void mergeInsertionSort(std::list<int>& container);
+
+		void formPairs(std::deque<int>& container, std::deque<Pair>& pairs, std::deque<long>& singleElements);
+		void merge(std::deque<Pair>& left, std::deque<Pair>& right, std::deque<Pair>& result);
+		void mergeSort(std::deque<Pair>& container);
+		void insertInMainChain(std::deque<int>& mainChain, std::deque<int>& pendElements);
+		void mergeInsertionSort(std::deque<int>& container);
 
 		template<typename T>
-		void merge(T& left, T& right);
-
-		template<typename T>
-		void mergeSort(T& container);
-
-        template<typename T>
-        void mergeInsertionSort(T& container);
+		T generateJacobsthalSequence(size_t size);
 
 		template<typename T>
 		bool isSorted(const T& container);
@@ -51,12 +60,7 @@ class PmergeMe
         void printContainer(T& container, const std::string& name, bool init);
 
 		std::list<int>::iterator findInsertPosition(std::list<int>& chain, int value);
-
-		int calculateInsertionCost(const std::list<int>& pendElements, int value);
-
-		int calculateNextJacobsthal();
-
-		void resetJacobsthal();
+		int findInsertPosition(std::deque<int>& mainChain, int value);
 
 	public:
 		PmergeMe(int argc, char* argv[]);
@@ -68,7 +72,6 @@ class PmergeMe
 		PmergeMe& operator=(const PmergeMe& other);
 
     	void sortAndPrint(void);
-		
 };
 
 #endif
